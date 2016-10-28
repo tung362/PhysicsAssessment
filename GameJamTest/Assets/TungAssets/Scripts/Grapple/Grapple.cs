6c3f457c84b78bdab0fc7686510d12159fd44437
Tung_Nguyen <7319tnguyen040@gmail.com>
2016-10-27 17:31:32 -0700

    WHAT????

--- GameJamTest/Assets/TungAssets/Scripts/Grapple/Grapple.cs
+++ GameJamTest/Assets/TungAssets/Scripts/Grapple/Grapple.cs
@@ -1,15 +1,58 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class Grapple : MonoBehaviour {
+public class Grapple : MonoBehaviour
+{
+    public GameObject TheLine;
+    public GameObject Test;
 
-	// Use this for initialization
-	void Start () {
+    //Last connection point = the real connection point, the rest are fallbacks
+    public List<Vector3> AnchorConnectionPoints;
+
+    private DistanceJoint2D TheDistanceJoint;
+
+
+    void Start ()
+    {
+        TheDistanceJoint = GetComponent<DistanceJoint2D>();
+    }
 	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+	void FixedUpdate()
+    {
+        //AnchorConnectionPoints.Clear();
+        //WallCheck(TheDistanceJoint.connectedAnchor, transform.position);
+        //AnchorConnectionPoints.Add(Vector2.zero);
+        //AnchorConnectionPoints[AnchorConnectionPoints.Count - 1
+
+        if (AnchorConnectionPoints.Count == 0) WallCheck(TheDistanceJoint.connectedAnchor, transform.position);
+        else
+        {
+            WallCheck(AnchorConnectionPoints[AnchorConnectionPoints.Count - 1], transform.position);
+            TheLine.GetComponent<LineRenderer>().SetVertexCount(AnchorConnectionPoints.Count + 1);
+            TheLine.GetComponent<LineRenderer>().SetPositions(AnchorConnectionPoints.ToArray());
+            TheLine.GetComponent<LineRenderer>().SetPosition(0, TheDistanceJoint.connectedAnchor);
+            TheLine.GetComponent<LineRenderer>().SetPosition(AnchorConnectionPoints.Count - 1, AnchorConnectionPoints[AnchorConnectionPoints.Count - 1]);
+            Test.transform.position = AnchorConnectionPoints[AnchorConnectionPoints.Count - 1];
+        }
+    }
+
+    void WallCheck(Vector3 Start, Vector3 End)
+    {
+        RaycastHit2D hit = Physics2D.Linecast(Start, End);
+        if (hit.transform.gameObject.name == "Platform")
+        {
+            //Vector2 hitPoint = Vector2.zero;
+
+            //if(TheDistanceJoint.connectedAnchor.x <= transform.position.x) hitPoint.x = hit.collider.bounds.max.x;
+            //else hitPoint.x = hit.collider.bounds.min.x;
+
+            //if (TheDistanceJoint.connectedAnchor.y <= transform.position.y) hitPoint.y = hit.collider.bounds.min.y;
+            //else hitPoint.y = hit.collider.bounds.max.y;
+
+            //AnchorConnectionPoints.Add(hitPoint);
+
+            AnchorConnectionPoints.Add(hit.point);
+        }
+    }
 }
--- GameJamTest/Assets/TungAssets/Scripts/Grapple/GrappleShoot.cs
+++ GameJamTest/Assets/TungAssets/Scripts/Grapple/GrappleShoot.cs
@@ -3,26 +3,23 @@ using System.Collections;
 
 public class GrappleShoot : MonoBehaviour
 {
+    //Temp
     public GameObject test;
+    private DistanceJoint2D TheDistanceJoint;
 
 	void Start ()
     {
-	
+        TheDistanceJoint = GetComponent<DistanceJoint2D>();
 	}
 	
-	void Update()
+	void FixedUpdate()
     {
         if (Input.GetMouseButtonDown(0))
         {
             Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit mouseHit;
 
-            if (Physics.Raycast(mouseRay, out mouseHit))
-            {
-                test.transform.position = new Vector3(mouseHit.point.x, mouseHit.point.y, 0);
-                //mouseHit.collider.GetComponent<Renderer>().material.color = Color.red;
-                //Debug.Log(mouseHit.transform.gameObject.name);
-            }
+            if (Physics.Raycast(mouseRay, out mouseHit)) TheDistanceJoint.connectedAnchor = new Vector3(mouseHit.point.x, mouseHit.point.y, 0);
         }
     }
 }

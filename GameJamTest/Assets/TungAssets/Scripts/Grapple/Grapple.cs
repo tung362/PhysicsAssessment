c66b46d7ea031405b6a36be9f0fb7c949782a4f1
Tung_Nguyen <7319tnguyen040@gmail.com>
2016-11-02 17:08:10 -0700

    Almost working box collider rope wrapping

--- GameJamTest/Assets/TungAssets/Scripts/Grapple/Grapple.cs
+++ GameJamTest/Assets/TungAssets/Scripts/Grapple/Grapple.cs
@@ -39,49 +39,95 @@ public class Grapple : MonoBehaviour
     }
 
     //Only supports box and circle colliders >:V
-    void WallCheck(Vector3 Start, Vector3 End)
+    void WallCheck(Vector2 Start, Vector2 End)
     {
         RaycastHit2D hit = Physics2D.Linecast(Start, End);
 
         Debug.DrawLine(Start, End);
-        if(hit.transform.GetComponent<CircleCollider2D>() != null)
-        {
-            Vector2 ObjectCenter = hit.transform.position;
-            Vector2 hitDirection = (ObjectCenter - hit.point).normalized;
-            AnchorConnectionPoints.Add(ObjectCenter - ((new Vector2(hitDirection.x * hit.transform.localScale.x, hitDirection.y * hit.transform.localScale.y)) * hit.transform.GetComponent<CircleCollider2D>().radius * 1.05f)); //Offset
-        }
-        else if(hit.transform.GetComponent<BoxCollider2D>() != null)
-        {
 
+        if (hit.transform.gameObject.name == "Platform")
+        {
+            if (hit.transform.GetComponent<CircleCollider2D>() != null)
+            {
+                Vector2 ObjectCenter = hit.transform.position;
+                Vector2 hitDirection = (ObjectCenter - hit.point).normalized;
+                AnchorConnectionPoints.Add(ObjectCenter - ((new Vector2(hitDirection.x * hit.transform.localScale.x, hitDirection.y * hit.transform.localScale.y)) * hit.transform.GetComponent<CircleCollider2D>().radius * 1.05f)); //Offset
+            }
+            else if (hit.transform.GetComponent<BoxCollider2D>() != null)
+            {
+                //Define edge points of the box collider
+                BoxCollider2D theCollider = hit.transform.GetComponent<BoxCollider2D>();
+                Vector2 size = theCollider.size;
+                Vector2 worldPosition = hit.transform.TransformPoint(theCollider.offset);
+
+                float up = (size.y / 2);
+                float down = -(size.y / 2);
+                float right = (size.x / 2);
+                float left = -(size.x / 2);
+
+                List<Vector2> points = new List<Vector2>();
+                points.Add(new Vector2(up, left));
+                points.Add(new Vector2(down, left));
+                points.Add(new Vector2(up, right));
+                points.Add(new Vector2(down, right));
+
+
+                //Find closest edge point
+                Vector3 closestVectice = Vector3.zero;
+                float closestDistance = int.MaxValue;
+                foreach (Vector2 point in points)
+                {
+                    Vector2 rotatedPoint = hit.transform.TransformDirection(point.x * (hit.transform.localScale.x + 0.055f),
+                                                                                        point.y * (hit.transform.localScale.y + 0.055f),
+                                                                                        transform.position.z);
+
+                    Vector2 pointWorldPosition = worldPosition + rotatedPoint;
+                    float distance = Vector2.Distance(hit.point, pointWorldPosition);
+
+                    if(distance < closestDistance && pointWorldPosition != Start)
+                    {
+                        closestDistance = distance;
+                        closestVectice = pointWorldPosition;
+                    }
+                }
+
+                AnchorConnectionPoints.Add(closestVectice);
+            }
+            //To do: support poly Collider
+            else if (hit.transform.GetComponent<PolygonCollider2D>() != null)
+            {
+
+            }
+
+            WallCheck(AnchorConnectionPoints[AnchorConnectionPoints.Count - 1], transform.position);
         }
-        //To do: support poly Collider
         //if (hit.transform.gameObject.name == "Platform")
         //{
         //    //(hit.normal * 0.01f)
         //    //AnchorConnectionPoints.Add(hit.point);
 
-        //    Vector3 closestVectice = Vector3.zero;
-        //    float closestDistance = int.MaxValue;
-        //    foreach(Vector3 point in hit.transform.GetComponent<MeshFilter>().mesh.vertices)
-        //    {
-        //        Vector3 rotatedPoint = hit.transform.TransformDirection(new Vector3(point.x * (hit.transform.localScale.x + 0.055f),
-        //                                                                            point.y * (hit.transform.localScale.y + 0.055f),
-        //                                                                            point.z * (hit.transform.localScale.z + 0.055f)));
+            //    Vector3 closestVectice = Vector3.zero;
+            //    float closestDistance = int.MaxValue;
+            //    foreach(Vector3 point in hit.transform.GetComponent<MeshFilter>().mesh.vertices)
+            //    {
+            //        Vector3 rotatedPoint = hit.transform.TransformDirection(new Vector3(point.x * (hit.transform.localScale.x + 0.055f),
+            //                                                                            point.y * (hit.transform.localScale.y + 0.055f),
+            //                                                                            point.z * (hit.transform.localScale.z + 0.055f)));
 
-        //        float distance = Vector2.Distance(hit.point, hit.transform.position + rotatedPoint);
-        //        Vector2 flatRotatedPoint = hit.transform.position + rotatedPoint;
+            //        float distance = Vector2.Distance(hit.point, hit.transform.position + rotatedPoint);
+            //        Vector2 flatRotatedPoint = hit.transform.position + rotatedPoint;
 
-        //        if (distance < closestDistance && flatRotatedPoint != new Vector2(Start.x, Start.y))
-        //        {
-        //            closestDistance = distance;
-        //            closestVectice = rotatedPoint;
-        //        }
-        //    }
+            //        if (distance < closestDistance && flatRotatedPoint != new Vector2(Start.x, Start.y))
+            //        {
+            //            closestDistance = distance;
+            //            closestVectice = rotatedPoint;
+            //        }
+            //    }
 
-        //    //Debug.Log((hit.transform.position + closestVectice).x);
-        //    Test.transform.position = hit.transform.position + closestVectice;
-        //    AnchorConnectionPoints.Add(hit.transform.position + closestVectice);
-        //}
+            //    //Debug.Log((hit.transform.position + closestVectice).x);
+            //    Test.transform.position = hit.transform.position + closestVectice;
+            //    AnchorConnectionPoints.Add(hit.transform.position + closestVectice);
+            //}
     }
 
     void Unwrap()
